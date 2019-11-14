245aea2d99a1857d281d6dc2f5beaa3020485e20
Tadej Vengust <vengust.tadej@gmail.com>
2019-11-14 14:29:24 +0100

    Fix tests.

--- src/tests/routes/get-root.test.ts
+++ src/tests/routes/get-root.test.ts
@@ -1,22 +1,36 @@
 import 'mocha';
-// import * as assert from 'assert';
-// import * as request from 'supertest';
-// import { ApiHttp, Stage } from '../..';
-// import env from '../../config/env';
+import * as assert from 'assert';
+import * as request from 'supertest';
+import { ApiHttp, Stage } from '../..';
+import env from '../../config/env';
 
 describe('routes/get-root', () => {
-  // const stage = new Stage(env);
-  // const api = new ApiHttp(stage);
 
   it('returns server info', async () => {
-    // const res = await request(api.app)
-    //   .get('/');
+    const stage = new Stage(env);
+    const api = new ApiHttp(stage);
 
-    // assert.equal(res.status, 200);
-    // assert.equal(res.body.data.name, '0xcert API');
-    // assert.equal(res.body.data.description, 'Open protocol for certified non-fungible tokens');
-    // assert.equal(res.body.data.uptime > 0, true);
-    // assert.equal(res.body.data.version, '0.1.0');
+    const res = await request(api.app)
+      .get('/');
+
+    assert.equal(res.status, 200);
+    assert.equal(res.body.data.name, '0xcert API');
+    assert.equal(res.body.data.description, 'Open protocol for certified non-fungible tokens.');
+    assert.equal(res.body.data.uptime > 0, true);
+    assert.equal(res.body.data.version, '0.1.0');
+  });
+
+  it('checks basic tests', async () => {
+    env.WEB3_URL='https://mainnet.infura.io/1jm7FU1sXc0lHFEYr83f';
+    const stage = new Stage(env);
+    await stage.connect();
+    const api = new ApiHttp(stage);
+
+    const res = await request(api.app)
+      .get('/basic?test=6&contract=0xf176d7bcdD07f8e474877095870685Ef0CCcCb2D');
+
+    
+      assert.equal(res.body.data, true);
   });
 
 });
